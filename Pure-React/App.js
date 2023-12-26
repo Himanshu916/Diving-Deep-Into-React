5cef56b95f165a0764fb9a4f52d2ddf6011ac62e
Himanshu Rana <hranaritsh@gmail.com>
2023-12-26 12:09:09 +0530

    learned-pure-react

---
+++ Pure-React/App.js
@@ -0,0 +1,9 @@
+// What
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("h1", { id: "heading" }, "Himanshu Rana here"),
+  React.createElement("p", {}, "learning pure react"),
+]);
+// where
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// put what into where
+root.render(parent);
